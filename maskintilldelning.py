8ef63434a02d2068c518500433d2697620a00280
Ida Gustafsson <idagusta@chalmers.se>
2026-02-13 15:34:34 +0100

    Lagt till algoritmen för maskintilldelning

---
+++ maskintilldelning.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 14:58:31 2026
+
+@author: idagu
+"""
+
+jobs = [] #tom lista med jobb
+p = {} #tom diconary med processing times, p[j]= pj
+m = 0 #antal maskiner
+machines = []
+classes = {} #dictionary som mappar jobb till dess klass/resurs
+
+def maskintilldelning(t: dict[int,float]) -> tuple[dict[int,int], dict[int,float]]:
+    #Maskintilldelningsschemat
+    sigma = {}
+    
+    p_m = {} #tom dictionary med maskinernas nuvarande totalbelastning
+    #Initiera totalbelastningen på varje maskin till 0
+    for i in machines: 
+        p_m.add(i,0)
+    
+    #Sortera jobben efter starttid
+    t_sorted = dict(sorted(t.items(), key=lambda item: item[1]))
+    
+    for j,t_j in t_sorted.items():
+        i = min(p_m, key=p_m.get) #maskinen med minst totalbelastning
+        sigma.add(j,i)
+    
+    return (sigma,t)
+        
+        
