0010e53f5119b11d76d1cc6d8a0a2ae33a0d9342
Ida Gustafsson <idagusta@chalmers.se>
2026-02-27 15:15:46 +0100

    Ändrat lite i maskintilldelningen

--- maskintilldelning.py
+++ maskintilldelning.py
@@ -4,22 +4,16 @@ Created on Fri Feb 13 14:58:31 2026
 
 @author: idagu
 """
+#t är en dict som mappar jobb till deras starttider
+#m är antalet maskiner
+#p är en dict som mappar jobb till deras processing times
 
-jobs = [] #tom lista med jobb
-p = {} #tom diconary med processing times, p[j]= pj
-machines = [] #tom lista med maskiner
-classes = {} #dictionary som mappar klasser till jobb
-
-def maskintilldelning(t: dict[int,float], machines: list[int], p: dict[int, float]) -> tuple[dict[int,int]]:
+def maskintilldelning(t, m, p):
     #Maskintilldelningsschemat
     sigma = {}
     
-    p_m = {i: 0 for i in machines}
-    
-   # p_m = {} #tom dictionary med maskinernas nuvarande totalbelastning
-    #Initiera totalbelastningen på varje maskin till 0
-    #for i in machines: 
-     #   p_m.add(i,0)
+    #Totalbelastning på varje maskin
+    p_m = {i: 0 for i in range(m)}
     
     #Sortera jobben efter starttid
     t_sorted = dict(sorted(t.items(), key=lambda item: item[1]))
