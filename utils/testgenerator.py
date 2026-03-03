51fbbdef0e47d39cdfb3dcd630bb200802902221
Ida Gustafsson <idagusta@chalmers.se>
2026-03-03 09:19:00 +0100

    Gjort ändringar i testgeneratorn

--- utils/testgenerator.py
+++ utils/testgenerator.py
@@ -7,8 +7,18 @@ Created on Fri Feb 27 13:02:04 2026
 import random
 import numpy as np
 
+def truncated_poisson(average, upper_bound):
+    while True:
+       # Slumpa ett tal från Poisson med medelvärde lam
+       value = np.random.poisson(average)
+       
+       # Kontrollera om det är inom intervallet [1, can_add]
+       if 1 <= value <= upper_bound:
+           return value
+
 def generate_class_processing_times(m,c,T): 
     target_sum = m*T
+    average_time = m*T/c
     class_processing_times = [1]*c
     remaining = target_sum - c
     
@@ -22,15 +32,20 @@ def generate_class_processing_times(m,c,T):
              if current_val < T:
                  # Lägg till så mycket som möjligt, men max upp till T
                  can_add = min(remaining, T - current_val)
+                 if can_add < average_time: 
+                     class_processing_times[i] += can_add
+                     remaining -=can_add
                  # Vi tar en slumpmässig del av det vi 'kan' lägga till för bättre varians
-                 add = random.randint(1, can_add)
-                 class_processing_times[i] += add
-                 remaining -= add
+                 else: 
+                     add = truncated_poisson(average_time, can_add)
+                     class_processing_times[i] += add
+                     remaining -= add
                  
     return class_processing_times
 
 def generate_number_of_jobs(n,c, class_processing_times): 
     target_sum = n
+    average_num = n/c
     num_jobs_in_each_class = [1]*c
     remaining = target_sum - c
     
@@ -44,9 +59,13 @@ def generate_number_of_jobs(n,c, class_processing_times):
             upper_bound = class_processing_times[i]
             if current_val < upper_bound:
                 can_add = min(remaining, upper_bound-current_val)
-                add = random.randint(1,can_add)
-                num_jobs_in_each_class[i] += add
-                remaining -= add
+                if can_add < average_num: 
+                    num_jobs_in_each_class[i]+= can_add
+                    remaining -=can_add
+                else: 
+                    add = truncated_poisson(average_num, upper_bound)
+                    num_jobs_in_each_class[i] += add
+                    remaining -= add
                 
     return num_jobs_in_each_class
 
@@ -78,8 +97,8 @@ def generate_number_of_jobs(n,c, class_processing_times):
 m = 3
 c = 5
 n = 20
+T = 30
 
-T = random.randint(np.ceil(m/n), 100) #kan ändra övre gränsen sen
 jobs= []
   
 class_processing_times = generate_class_processing_times(m, c, T)
